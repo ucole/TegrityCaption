4165ad0e7a1ca91599560d74ec1b11172fe0d108
Unika Cole <unika.cole@knights.ucf.edu>
2013-03-20 10:21:32 -0400

    V 1.0

---
+++ TegrityCaption/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TegrityCaption
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Upload = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Upload
+            // 
+            this.Upload.Location = new System.Drawing.Point(60, 28);
+            this.Upload.Name = "Upload";
+            this.Upload.Size = new System.Drawing.Size(75, 23);
+            this.Upload.TabIndex = 0;
+            this.Upload.Text = "Upload File";
+            this.Upload.UseVisualStyleBackColor = true;
+            this.Upload.Click += new System.EventHandler(this.Upload_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 92);
+            this.Controls.Add(this.Upload);
+            this.Name = "Form1";
+            this.Text = "Caption Uploader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Upload;
+    }
+}
+
---
+++ TegrityCaption/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace TegrityCaption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Upload_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();//show dialog box
+            if (result == DialogResult.OK)
+            {
+                
+                StreamWriter filename = new StreamWriter("New_Caption.txt");   
+                foreach (String line in File.ReadAllLines(openFileDialog1.FileName))
+                {
+                    if (line.Length > 0)
+                    {
+                        String trimFile = line.Trim();//WORKS!
+                        //split file
+                        String time = "["+ trimFile.Substring(0, 8) + ".000]";
+                        trimFile = trimFile.Remove(0,8);
+                        //Replace ampersan with "and"
+                        trimFile = trimFile.Replace("&", "and");//Works!
+                        //trim end of file
+                        trimFile = trimFile.TrimEnd();
+                        //take care of non-ASCII characters 
+                        //foreach (char c in trimFile)
+                        //{
+                        //    int ascii = Convert.ToInt32(trimFile);
+                        //    if (ascii > 127)
+                        //    {
+                        //        trimFile.Replace(c.ToString(), "");
+                        //    }
+                        //}
+                        filename.WriteLine(time + trimFile);
+                    }
+                }
+                filename.Close();
+            }
+        }
+    }//end of Form1
+}
